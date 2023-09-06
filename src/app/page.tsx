dd851a3cbf8dd58667a091f7db6bd7732a7f9385
AntoniaGranit <antonia.granit@gmail.com>
2023-09-06 16:00:38 +0300

    install dotenv and add backend api url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import About from './about/page'
+import Image from 'next/image';
+import About from './about/page';
+
 
 export default function Home() {
   return (
