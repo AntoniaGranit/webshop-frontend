d89229020e12eae790e522b6ffb28053df1546ba
AntoniaGranit <antonia.granit@gmail.com>
2023-09-08 14:50:14 +0300

    add text to about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,19 @@
 const About: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">About</h1>
+        <h1 className="text-4xl font-bold">About Us</h1>
+        <p>At LeafLife, we&apos;re passionate about bringing the beauty and joy of houseplants into your life. Whether you&apos;re a seasoned plant enthusiast or just starting your green journey, we&apos;re here to make your indoor gardening experience a breeze!</p>
+        <h2 className="font-bold">Our Story</h2>
+        <p>LeafLife sprouted from a shared love for plants among a group of friends who believed that everyone should have access to lush, vibrant greenery in their homes. Inspired by our own experiences with houseplants and the positive impact they&apos;ve had on our lives, we set out to create a place where plant lovers can find everything they need.</p>
+        <h2 className="font-bold">Our Mission</h2>
+        <p>Our mission is simple: to help you create a greener, more vibrant space that you can call home. We&apos;re committed to providing you with the finest selection of houseplants, expert care tips, and the tools you need to nurture your indoor garden.</p>
+        <h2 className="font-bold">What Sets Us Apart</h2>
+        <ul className="list-disc">
+        <li><b>Quality Assurance:</b> We handpick each plant to ensure it meets our rigorous quality standards, so you can trust that you're getting the best.</li>
+        <li><b>Expert Advice:</b> Our team of plant enthusiasts and horticultural experts is here to answer your questions and provide guidance every step of the way.</li>
+        <li><b>Sustainability:</b> We care about the environment and strive to be eco-conscious in all our practices, from packaging to sourcing.</li>
+        <li><b>Community:</b> Join our growing community of plant lovers on social media and share your LeafLife journey with us.</li>
+        </ul>
     </main>
   )
 }
