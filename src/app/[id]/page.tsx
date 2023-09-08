7b0e3c7470b821f615467d0d6e78e3cb4c862c04
AntoniaGranit <antonia.granit@gmail.com>
2023-09-08 13:34:17 +0300

    reaching dynamic route now but id is null

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,16 @@
-import { useRouter } from 'next/router';
+'use client';
+import { useSearchParams } from 'next/navigation'
 import SingleProduct from '../components/SingleProduct';
 
 const SingleProductPage: React.FC = () => {
-  const router = useRouter();
-  const { id } = router.query; 
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id');
+  console.log('id:', id);
 
     // Check if id is undefined or null, and handle it accordingly
     if (!id) {
         // Handle the case when the id is not provided in the URL
-        return <div>Plant not found.</div>;
+        return <div>Plant not found!!!!!!!!!</div>;
       }
 
   return <SingleProduct id={id} />;
