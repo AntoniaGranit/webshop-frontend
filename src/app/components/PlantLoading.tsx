2946811aeb1842c290017213792e139c789b4aa2
AntoniaGranit <antonia.granit@gmail.com>
2023-09-26 21:31:54 +0300

    install redux toolkit and create reducer structure

--- src/app/components/PlantLoading.tsx
+++ src/app/components/PlantLoading.tsx
@@ -6,21 +6,20 @@ const Loading: React.FC = () => {
           <div className='rounded-l bg-gray-200 h-400 w-400' />
         </div>
         <div className='flex-column w-8/12 self-center'>
-          <div className='flex-column pb-4'>
-            <p className={`text-2xl font-semibold pb-1`}>blablabla</p>
-            <div className='flex-column border-t border-gray-200 pb-3' />
-            <p>blablabla</p>
-            <p>blablabla</p>
-          </div>
-          <div className='pb-4'>
-            <p></p>
-          </div>
-          <div className='pb-3'>
-            <p>Amount:</p>
-            <div className='block w-3/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset'></div>
-          </div>
-          <div className='bg-gray-200 text-gray-200 font-bold py-2 px-4 rounded-xl'>
-            Add to cart
+          <div className='shadow rounded-md p-4 max-w-sm w-full mx-auto'>
+            <div className='animate-pulse flex space-x-4'>
+              <div className='rounded-full bg-slate-200 h-10 w-10'></div>
+              <div className='flex-1 space-y-6 py-1'>
+                <div className='h-2 bg-slate-300 rounded'></div>
+                <div className='space-y-3'>
+                  <div className='grid grid-cols-3 gap-4'>
+                    <div className='h-2 bg-slate-300 rounded col-span-2'></div>
+                    <div className='h-2 bg-slate-300 rounded col-span-1'></div>
+                  </div>
+                  <div className='h-2 bg-slate-300 rounded'></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
